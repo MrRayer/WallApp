1a12b298db8556479e53c13ac3ff47492b24f460
MrRayer <rodrigo.ces.b@gmail.com>
2023-01-08 13:50:28 -0300

    firstCommit

---
+++ Controllers/UserPostsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserPostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserPostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserPosts
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.UserPost.ToListAsync());
+        }
+
+        // GET: UserPosts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserPost == null)
+            {
+                return NotFound();
+            }
+
+            var userPost = await _context.UserPost
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (userPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(userPost);
+        }
+
+        // GET: UserPosts/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserPosts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,PostUser,PostTime,PostContents")] UserPost userPost)
+        {
+            if (ModelState.IsValid)
+            {
+                userPost.PostTime = DateTime.Now;
+                _context.Add(userPost);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userPost);
+        }
+
+        // GET: UserPosts/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserPost == null)
+            {
+                return NotFound();
+            }
+
+            var userPost = await _context.UserPost.FindAsync(id);
+            if (userPost == null)
+            {
+                return NotFound();
+            }
+            return View(userPost);
+        }
+
+        // POST: UserPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,PostUser,PostTime,PostContents")] UserPost userPost)
+        {
+            if (id != userPost.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userPost);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserPostExists(userPost.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userPost);
+        }
+
+        // GET: UserPosts/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserPost == null)
+            {
+                return NotFound();
+            }
+
+            var userPost = await _context.UserPost
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (userPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(userPost);
+        }
+
+        // POST: UserPosts/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserPost == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.UserPost'  is null.");
+            }
+            var userPost = await _context.UserPost.FindAsync(id);
+            if (userPost != null)
+            {
+                _context.UserPost.Remove(userPost);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserPostExists(int id)
+        {
+          return _context.UserPost.Any(e => e.id == id);
+        }
+    }
+}
---
+++ Models/UserPost.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserPost
+    {
+        public int id { get; set; }
+        public string PostUser { get; set; }
+        public DateTime PostTime { get; set; }
+        public string PostContents { get; set; }
+
+        public UserPost()
+        {
+
+        }
+    }
+}
